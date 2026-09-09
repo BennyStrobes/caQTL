092dc6ec27c9d5edceaf3da4989d1cf280dd8369
Ben Strober <bstrober3@gmail.com>
2026-09-09 14:37:01 -0400

    first commit

---
+++ exploratory/driver_key.sh
@@ -0,0 +1,21 @@
+###############
+# Input data
+###############
+
+# Directory containing fingen multiome data
+fingen_data_dir="/lab-share/CHIP-Strober-e2/Public/finngen/public_multiome/finngen_multiome/"
+
+# eQTL data dir
+fingen_eqtl_dir=${fingen_data_dir}"eQTL/cis_nominal/"
+
+# caQTL data dir
+fingen_caqtl_dir=${fingen_data_dir}"caQTL/cis_nominal/"
+
+# peak-gene links data
+fingen_peak_gene_links_dir=${fingen_data_dir}"peak_gene_links/"
+
+###############
+# Output directories
+###############
+output_root="/lab-share/CHIP-Strober-e2/Public/ben/caQTL/exploratory_analyses/"
+
